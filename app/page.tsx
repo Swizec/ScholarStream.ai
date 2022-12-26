efaf0befe6180e3cfa0152457d57d73017e3d4ce
Swizec Teller <swizec@swizec.com>
2022-12-26 15:16:43 +0100

    fix styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import "@/styles/globals.css";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
