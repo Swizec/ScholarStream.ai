67f2ccd61faccef80737f92d3ac483201efcc578
Swizec Teller <swizec@swizec.com>
2022-12-26 15:15:43 +0100

    Move to new app/

---
+++ app/head.tsx
@@ -0,0 +1,20 @@
+import "@/styles/globals.css";
+
+export default function Head() {
+    return (
+        <>
+            <title>Untitled</title>
+            <meta charSet="UTF-8" />
+            <meta
+                name="description"
+                content="
+                A twitter-like reading experience for arXiv feeds using GPT"
+            />
+            <meta name="author" content="Swizec Teller" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+        </>
+    );
+}
