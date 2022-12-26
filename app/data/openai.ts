a30d3f020faed709ca6c2558de44705adeded90d
Swizec Teller <swizec@swizec.com>
2022-12-26 18:34:05 +0100

    Cache OpenAI summaries

--- app/data/openai.ts
+++ app/data/openai.ts
@@ -1,21 +1,31 @@
 import { Configuration, OpenAIApi } from "openai";
 import { ArxivFeedItem } from "./arxiv";
+import { Redis } from "@upstash/redis";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+const redis = Redis.fromEnv();
 
 export async function getSummary(paper: ArxivFeedItem) {
-    const completion = await openai.createCompletion({
-        // model: "text-davinci-002",
-        model: "text-davinci-003",
-        prompt: createPrompt(paper.contentSnippet),
-        temperature: 0.6,
-        max_tokens: 100,
-    });
+    const cacheKey = `summary-cache:${paper.link}`;
+    const cached = await redis.get(cacheKey);
 
-    return completion.data;
+    if (cached) {
+        return cached;
+    } else {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: createPrompt(paper.contentSnippet),
+            temperature: 0.6,
+            max_tokens: 100,
+        });
+
+        await redis.set(cacheKey, completion.data);
+
+        return completion.data;
+    }
 }
 
 function createPrompt(abstract: string) {
