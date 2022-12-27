b387ecc7ba72cccc7b73f5d4bc9e91d642ec7eb6
Swizec Teller <swizec@swizec.com>
2022-12-27 16:59:02 +0100

    Show authors and link to paper

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,43 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import * as arxiv from "./data/arxiv";
 import * as openai from "./data/openai";
+import { CreateCompletionResponse } from "openai";
+
+const FeedItem = (props: {
+    paper: arxiv.ArxivFeedItem;
+    summary: CreateCompletionResponse;
+}) => {
+    const { paper, summary } = props;
+
+    return (
+        <div key={summary.id} style={{ marginBottom: "1em" }}>
+            <h3
+                dangerouslySetInnerHTML={{
+                    __html: paper.creator.replace(/<[^>]*>?/gm, ""),
+                }}
+            />
+            <p>{summary.choices[0].text}</p>
+            <a href={paper.link} style={{ color: "blue" }}>
+                {paper.link}
+            </a>
+        </div>
+    );
+};
 
 export default async function Home() {
-    const feed = await arxiv.getFeed("cs.SE");
+    const feed = await arxiv.getFeed("cs");
     const papers = feed.items.slice(0, 10);
+    console.log(feed.items.length);
 
-    const summaries = await Promise.all(papers.map(openai.getSummary));
+    const summaries: [arxiv.ArxivFeedItem, CreateCompletionResponse][] =
+        await Promise.all(
+            papers.map(async (paper) => [paper, await openai.getSummary(paper)])
+        );
 
     return (
         <main className={styles.main}>
-            {summaries.map((summary) => (
-                <div key={summary.id} className={styles.description}>
-                    <p>{summary.choices[0].text}</p>
-                </div>
+            {summaries.map(([paper, summary]) => (
+                <FeedItem paper={paper} summary={summary} key={summary.id} />
             ))}
         </main>
     );
