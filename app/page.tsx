9fe346dbcbe85ee62a887f27d247433c2f21d898
Swizec Teller <swizec@swizec.com>
2023-01-04 19:07:21 -0800

    Add ChatGPT generated pitch

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,8 @@ const FeedItem = (props: {
     );
 };
 
-export default async function Home() {
-    const feed = await arxiv.getFeed("econ");
+const Feed = async (props: { topic: string }) => {
+    const feed = await arxiv.getFeed(props.topic);
     const papers = feed.items.slice(0, 10);
 
     const summaries: [arxiv.ArxivFeedItem, CreateCompletionResponse][] =
@@ -46,10 +46,49 @@ export default async function Home() {
         );
 
     return (
-        <main className={styles.main}>
+        <div className={feedStyles.feed}>
+            <h1>Latest "{props.topic}" papers</h1>
             {summaries.map(([paper, summary]) => (
                 <FeedItem paper={paper} summary={summary} key={summary.id} />
             ))}
+        </div>
+    );
+};
+
+const Pitch = () => (
+    <div className={styles.pitch}>
+        <h1>ScholarStream.ai</h1>
+        <p>Welcome to the world of academic insights!</p>
+        <p>
+            Are you tired of the shallow and attention-seeking content on social
+            media? Are you looking for a deeper understanding of the world
+            around you? Look no further!
+        </p>
+        <p>
+            ScholarStream.ai brings you the latest research from the world of
+            academia, all in a convenient and engaging Twitter-like feed.
+        </p>
+        <p>
+            With ScholarStream.ai, you can dive into the latest findings from
+            top researchers and scholars, all at the touch of a button. No more
+            sifting through tedious journals or abstracts – our AI does the work
+            for you, presenting you with the latest information in an easily
+            digestible format.
+        </p>
+        <p>
+            Explore the depths of knowledge today with ScholarStream.ai. Your
+            mind will thank you!
+        </p>
+    </div>
+);
+
+export default async function Home() {
+    return (
+        <main className={styles.main}>
+            <Pitch />
+
+            {/* @ts-expect-error Server Component */}
+            <Feed topic="econ" />
         </main>
     );
 }
